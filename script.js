6b8a6fae77730a5a3b500dbe66e6bed0a5499aa4
55746 <Lawhead.gavin@live.jobcorps.org>
2022-08-10 18:47:47 +0000

    fixed the error

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-function click() {
-  let city = document.querySelector(".inputValue").value;
-  let url =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    { city } +
-    "&appid=ea395fc5637d0b6cdcbda1eb26226234&units=imperial";
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      data = document.querySelector(".name").innertext;
-    });
-  // document.querySelector(".name") = url.name
-}
+const city = document.querySelector(".inputValue");
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?q=" +
+  { city } +
+  "&appid=ea395fc5637d0b6cdcbda1eb26226234&units=imperial";
+
+fetch(url)
+  .then((response) => response.json())
+  .then((location) => console.log(location))
+  .catch((error) => console.log(error));
+
+//  "https://api.openweathermap.org/data/2.5/weather?q=" +
+//  { city } +
+//  "&appid=ea395fc5637d0b6cdcbda1eb26226234&units=imperial";
