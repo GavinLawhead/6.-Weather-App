f65e517234c3f760f81dcac54302a6120d0bd5d0
55746 <Lawhead.gavin@live.jobcorps.org>
2022-08-10 18:04:39 +0000

    first commit

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-let button = document.querySelector(".button");
-let inputValue = document.querySelector(".inputValue");
-let name = document.querySelector(".name");
-let desc = document.querySelector(".desc");
-let temp = document.querySelector(".temp");
-
 function click() {
-  fetch(
-    "https://api.openweathermap.org/data/3.0/weather?q=" +
-      inputValue.value +
-      "&appid={ea395fc5637d0b6cdcbda1eb26226234}"
-  )
+  let city = document.querySelector(".inputValue").value;
+  let url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    { city } +
+    "&appid=ea395fc5637d0b6cdcbda1eb26226234&units=imperial";
+  fetch(url)
     .then((response) => response.json())
-    .then(data + console.log(data))
-
-    .catch((err) => alert("wrong city name"));
+    .then((data) => {
+      data = document.querySelector(".name").innertext;
+    });
+  // document.querySelector(".name") = url.name
 }
