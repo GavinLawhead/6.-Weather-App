0f36c0885d20e73b1303e91723c3e34093215e4b
55746 <Lawhead.gavin@live.jobcorps.org>
2022-08-12 17:03:41 +0000

    FIGURED IT OUt, just finish the script

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const city = document.querySelector(".inputValue");
-const url =
-  "https://api.openweathermap.org/data/2.5/weather?q=" +
-  { city } +
-  "&appid=ea395fc5637d0b6cdcbda1eb26226234&units=imperial";
-
-fetch(url)
-  .then((response) => response.json())
-  .then((location) => console.log(location))
-  .catch((error) => console.log(error));
-
-//  "https://api.openweathermap.org/data/2.5/weather?q=" +
-//  { city } +
-//  "&appid=ea395fc5637d0b6cdcbda1eb26226234&units=imperial";
+let temp = "";
+function thisfunc() {
+  const city = document.querySelector(".inputValue").value;
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=ea395fc5637d0b6cdcbda1eb26226234&units=imperial"
+  )
+    .then((response) => response.json())
+    .then((location) => {
+      temp = location.main.temp;
+      console.log(location);
+      document.querySelector(".temp").innerText = temp;
+    })
+    .catch((error) => console.log("error"));
+}
